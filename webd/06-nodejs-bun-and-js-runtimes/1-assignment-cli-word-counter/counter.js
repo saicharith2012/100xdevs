7ca004aec50611f963ad7ffd76662e35b5c36812
saicharith2012 <saicharithpalabindhela@gmail.com>
2025-01-25 15:26:34 +0530

    modularising into different commands and functions

--- webd/06-nodejs-bun-and-js-runtimes/1-assignment-cli-word-counter/counter.js
+++ webd/06-nodejs-bun-and-js-runtimes/1-assignment-cli-word-counter/counter.js
@@ -4,34 +4,67 @@ const { Command } = require("commander");
 const fs = require("fs");
 const program = new Command();
 
+function readFileAndOperate(filepath, operation) {
+  fs.readFile(filepath, "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    if (operation === "r") {
+      console.log("file content:\n\n" + data);
+      return;
+    }
+
+    const lines = data.split("\n");
+    if (operation === "l") {
+      console.log(`You have ${lines.length} lines in this file.`);
+      return;
+    }
+
+    let wordCount = 0;
+    if (operation === "w") {
+      lines.forEach((line) => {
+        wordCount += line.split(" ").length;
+      });
+
+      console.log(`You have ${wordCount} words in this file.`);
+      return;
+    }
+  });
+}
+
 program
-  .name("count-no-of-words")
+  .name("counter")
   .description("CLI to count the no.of words in a file.")
   .version("1.0.0");
 
+program.command;
+
 program
-  .command("count")
-  .description("count the lines and words")
+  .command("file-content")
+  .description("outputs the file content")
   .argument("<filepath>", "path to the file")
   .action((filepath) => {
-    fs.readFile(filepath, "utf-8", (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(data)
-        const lines = data.split("\n")
-        const lineCount = lines.length
-        let wordCount = 0;
-        lines.forEach((line) => {
-          const words = line.split(" ")
-          wordCount += words.length
-        })
-        console.log(`You have ${wordCount} words and ${lineCount} lines in this file`);
-      }
-    });
+    readFileAndOperate(filepath, "r");
   });
 
-program.parse()
+program
+  .command("count-lines")
+  .description("counts the number of lines in the provided file.")
+  .argument("<filepath>", "path to the file")
+  .action((filepath) => {
+    readFileAndOperate(filepath, "l");
+  });
+
+program
+  .command("count-words")
+  .description("counts the number of words in the provided file.")
+  .argument("<filepath>", "path to the file")
+  .action((filepath) => {
+    readFileAndOperate(filepath, "w");
+  });
+
+program.parse();
 
 // to make the script executable
 // chmod +x wordCounter.js
